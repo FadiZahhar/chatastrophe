6a1695acbc3a6500fbd3dd7cda334733b38bec9e
Fadi Zahhar <79freelancer@gmail.com>
2019-02-19 16:20:54 +0000

    working on chapter 3

---
+++ Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div id="Header">
+            <img src="/assets/icon.png" alt="logo" />
+            <h1>Chatastrophe</h1>
+        </div>
+        );
+};
+
+export default header;
